f00175af57782708e9ac0433f52662bd9f80d3e8
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2023-07-26 00:21:39 +0900

    babel 사용법 연습, readme 참조

---
+++ new.js
@@ -0,0 +1,7 @@
+// $ npx babel test.js -o new.js
+
+"use strict";
+
+[1, 2, 3].map(function (n) {
+  return n + 1;
+});
---
+++ test.js
@@ -0,0 +1,8 @@
+[1, 2, 3].map((n) => n + 1);
+
+// $ npx babel test.js
+// "use strict";
+
+// [1, 2, 3].map(function (n) {
+//   return n + 1;
+// });
